4363bd1b53365bc51a57dccd792aad78cb855546
Vinicius Tartari <vinitartari@gmail.com>
2019-05-14 16:37:14 -0300

    Add controller de log

---
+++ Backend/src/controllers/LogController.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const Log = mongoose.model("Log");
+
+module.exports = {
+  /**
+   * LOG - FUNCTIONS
+   * Create and Read only
+   */
+  //Create
+  async createImageBitmap(req, res) {
+    const log = await Log.create(req.body);
+    return res.json(log);
+  },
+
+  //Read
+  async read(req, res) {
+    if ("id" in req.params) {
+      const log = await Log.findById(req.params.id);
+      return res.json(log);
+    } else {
+      const log = await Log.find({});
+      return res.json(log);
+    }
+  }
+};
