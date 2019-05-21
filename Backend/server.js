0b15f3af63cc91c2e7419d0d1f1e6d2472239b52
Vinicius Tartari <vinitartari@gmail.com>
2019-05-21 11:11:35 -0300

    Atualizado esquema de gravacao de log

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors"); //acesso de outros domínios
 const mongoose = require("mongoose"); //conexão com o mongoDB
 const requireDir = require("require-dir");
+const axios = require("axios");
+const morgan = require("morgan");
 const mosca = require("mosca");
 
 //Iniciando o app
@@ -25,23 +27,8 @@ const moscaSettings = {
   }
 };
 
-const server = new mosca.Server(moscaSettings);
-server.on("ready", setup);
-
-// Dispara quando um cliente se conecta
-server.on("clientConnected", function(client) {
-  // console.log("Client Connected:", client.id);
-});
-
-// Dispara quando um cliente se desconecta
-server.on("clientDisconnected", function(client) {
-  // console.log("Client Disconnected:", client.id);
-});
-
-// Dispara quando uma mesnsagem eh publicada
-server.on("published", function(packet, client) {
-  // console.log("Published:\n", packet);
-});
+const moscaServer = new mosca.Server(moscaSettings);
+moscaServer.on("ready", setup);
 
 function setup() {
   console.log("Mosca server is up and running!");
@@ -58,6 +45,9 @@ Pode receber parâmetros como domínios que podem acessar, entre outros;
 */
 app.use(cors());
 
+// Mostra as requisicoes http que o servidor receber no console;
+app.use(morgan("dev"));
+
 //Iniciando o DB
 mongoose.connect("mongodb://localhost:27017/tcc", { useNewUrlParser: true });
 
@@ -68,3 +58,102 @@ requireDir("./src/models");
 app.use("/api", require("./src/routes"));
 
 app.listen(3000);
+
+//test
+// Colocar acesso direto ao banco aqui...
+const LogController = require("./src/controllers/LogController");
+
+chamaApi = log => {
+  axios
+    .request({
+      method: "post",
+      url: "http://localhost:3000/api/log",
+      data: log
+    })
+    .catch(err => console.log(err));
+};
+
+// Dispara quando um cliente se conecta
+moscaServer.on("clientConnected", function(packet) {
+  // console.log("Client Connected:", client.id);
+  // Enviar para log
+  const log = {
+    log_type: "connect",
+    log_client: packet.id
+  };
+  chamaApi(log);
+
+  console.log("Cliente " + log.log_client + " conectado!");
+});
+
+// Dispara quando um cliente se desconecta
+moscaServer.on("clientDisconnected", function(packet) {
+  // console.log("Client Disconnected:", client.id);
+  // enviar para log
+  const log = {
+    log_type: "disconnect",
+    log_client: packet.id
+  };
+  chamaApi(log);
+
+  console.log("Cliente " + log.log_client + " desconectado!");
+});
+
+// Dispara quando uma mensagem eh publicada
+moscaServer.on("published", function(packet) {
+  if (packet.topic.includes("$SYS")) {
+    if (packet.topic == "$SYS/mosca/new/subscribes") {
+      const payload = JSON.parse(packet.payload);
+      const log = {
+        log_type: "subscribe",
+        log_client: payload.clientId,
+        log_topic: payload.topic,
+        log_messageId: packet.messageId
+      };
+      chamaApi(log);
+
+      console.log(
+        "Cliente " +
+          log.log_client +
+          " se inscreveu no topico " +
+          log.log_topic +
+          "!"
+      );
+    } else if (packet.topic == "$SYS/mosca/new/unsubscribes") {
+      const payload = JSON.parse(packet.payload);
+      const log = {
+        log_type: "unsubscribe",
+        log_client: payload.clientId,
+        log_topic: packet.topic,
+        log_messageId: packet.messageId
+      };
+      chamaApi(log);
+
+      console.log(
+        "Cliente " +
+          log.log_client +
+          " se desinscreveu do topico " +
+          log.log_topic +
+          "!"
+      );
+    }
+  } else {
+    const log = {
+      log_type: "publish",
+      log_topic: packet.topic,
+      log_messageId: packet.messageId,
+      // Converte o payload de Buffer para String
+      log_payload: packet.payload.toString("utf8")
+    };
+    chamaApi(log);
+    console.log(
+      "Publicado no topico " +
+        log.log_topic +
+        " o valor " +
+        log.log_payload +
+        "!"
+    );
+  }
+
+  // enviar para tratamento
+});
