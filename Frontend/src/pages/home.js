4589b268d33ea8d02d68cb12ab59af00757de3c6
Vinicius Tartari <vinitartari@gmail.com>
2019-06-09 18:15:44 -0300

    Add layout da pagina home

--- Frontend/src/pages/home.js
+++ Frontend/src/pages/home.js
@@ -1,11 +1,23 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 export default class Home extends Component {
   render() {
     return (
       <div>
-        <h1>Welcome!</h1>
-        <h4>Go to login...</h4>
+        <Navbar />
+        <div id="wrapper">
+          <div className="container-fluid">
+            <h1>Welcome!</h1>
+            <h4>
+              Go to <Link to="/login">Login</Link>
+            </h4>
+          </div>
+        </div>
+        <Footer />
       </div>
     );
   }
